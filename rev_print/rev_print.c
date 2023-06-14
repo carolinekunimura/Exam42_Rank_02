51babff70bba7c67780956dc8f0bb96f24834803
Caroline Kunimura <carolinekunimura@gmail.com>
2023-06-14 20:05:58 +0000

    finish

--- rev_print/rev_print.c
+++ rev_print/rev_print.c
@@ -1,25 +1,16 @@
 #include <unistd.h>
 
-int	ft_strlen(char *str)
-{
-	int i = 0;
-	while (str[i] != '\0')
-		i++;
-	return(i);	
-}
-
 int	main(int argc, char **argv)
 {
-	int	i = ft_strlen(argv[1]) - 1;
+	int	i = 0;
 	
-	if (argc > 1)
+	if (argc == 2)
 	{
-		while(i >= 0)
-		{
-			write(1, &argv[1][i], 1);
-			i--;
-		}
+		while(argv[1][i])
+			i++;
+		while(i)
+			write(1, &argv[1][--i], 1);
 	}
 	write(1, "\n", 1);
-	return(0);
+	return (0);
 }
