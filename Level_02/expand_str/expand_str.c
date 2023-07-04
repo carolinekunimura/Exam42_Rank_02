a32c84c983367abd5b5e57774318343b1a9175a4
Caroline Kunimura Plaza <ckunimur@c1r1p1.42sp.org.br>
2023-07-04 13:12:30 -0300

    new one

---
+++ Level_02/expand_str/expand_str.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_str.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckunimur <ckunimur@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 18:54:26 by ckunimur          #+#    #+#             */
+/*   Updated: 2023/07/04 13:11:58 by ckunimur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int flag = 0;
+	
+	if (argc == 2)
+	{
+		while(argv[1][i] == ' ' || argv[1][i] == '\t')
+			i++;
+		while( argv[1][i])
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+				flag = 1;
+			if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
+			{
+				if (flag)
+					write(1, "   ", 3);
+				flag = 0;
+				write(1, &argv[1][i], 1);
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
